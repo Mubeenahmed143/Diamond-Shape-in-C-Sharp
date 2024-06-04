c3bb710f2e4a3d5bf87d7f8b7a46c6052416c93c
Mubeenahmed143 <ahmedsheikhmubeen395@gmail.com>
2024-06-05 01:26:14 +0500

    Add project files.

---
+++ Diamond/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Making a Diamond Shape");
+
+for(int a=1;  a<=5;  a++)
+{
+    for(int b=5-a;  b>0;  b--)
+    {
+        Console.Write(" ");
+    }
+        for(int c=1; c<=a; c++)
+        {
+            Console.Write("* ");
+        }
+    Console.WriteLine();
+}
+for(int i=5-1; i>=1; i--)
+{
+    for (int j = 5 - i; j > 0; j--)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 1; k <= i; k++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
+
